929f98a1fb3a15d1930a6cbb5afb0e30b414b248
citycide <thecitycide@gmail.com>
2016-08-08 00:06:19 -0500

    docs(initial): add jsdoc generation

--- rollup.config.js
+++ rollup.config.js
@@ -7,6 +7,11 @@ export default {
   entry: 'lib/index.js',
   plugins: [
     babel({
+      babelrc: false,
+      presets: ['es2015-rollup', 'stage-0'],
+      plugins: [
+        'transform-runtime'
+      ],
       externalHelpers: false,
       runtimeHelpers: true
     })
