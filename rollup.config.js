3f1b1f34a5db15f81f2a0bb5cdaa032f9cd7caa9
citycide <thecitycide@gmail.com>
2016-08-23 01:25:01 -0500

    chore(build): rollback build methods
    Undo adding the commonjs & node-resolve plugins. Re-externalize everything. 
    
    I'm a super fool.

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,4 @@
 import babel from 'rollup-plugin-babel'
-import cjs from 'rollup-plugin-commonjs'
-import resolve from 'rollup-plugin-node-resolve'
 
 const pkg = require('./package.json')
 const external = Object.keys(pkg.dependencies)
@@ -9,8 +7,6 @@ export default {
   entry: 'src/index.js',
   external,
   plugins: [
-    resolve({ jsnext: true, main: true }),
-    cjs({ exclude: 'src/**' }),
     babel({
       babelrc: false,
       presets: [
@@ -22,6 +18,7 @@ export default {
         'transform-flow-strip-types',
         'transform-runtime'
       ],
+      externalHelpers: false,
       runtimeHelpers: true,
       exclude: 'node_modules/**'
     })
