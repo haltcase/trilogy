d5514e16cc878a93ed588bc02a4323f5d4c067a0
citycide <lingenbw@gmail.com>
2021-08-07 20:49:14 -0500

    fix: update sql.js type imports
    Closes #109

--- src/sqljs-handler.ts
+++ src/sqljs-handler.ts
@@ -6,13 +6,13 @@ import { createPool, Pool } from 'generic-pool'
 import { Trilogy } from '.'
 import { makeDirPath } from './util'
 
-import { SqlJs } from 'sql.js/module'
+import { Database } from 'sql.js'
 
-export async function readDatabase (instance: Trilogy): Promise<SqlJs.Database> {
+export async function readDatabase (instance: Trilogy): Promise<Database> {
   const name = instance.options.connection.filename
 
   const init = await import('sql.js')
-  const SQL = await init()
+  const SQL = await init.default()
 
   if (name === ':memory:') {
     return new SQL.Database()
@@ -36,7 +36,7 @@ export async function readDatabase (instance: Trilogy): Promise<SqlJs.Database> 
   return client
 }
 
-export function writeDatabase (instance: Trilogy, db: SqlJs.Database) {
+export function writeDatabase (instance: Trilogy, db: Database) {
   const name = instance.options.connection.filename
   if (name === ':memory:') return
 
@@ -44,13 +44,13 @@ export function writeDatabase (instance: Trilogy, db: SqlJs.Database) {
   writeFileSync(name, db.export(), { mode: parseInt('0777', 8) })
 }
 
-export function pureConnect (instance: Trilogy): Pool<SqlJs.Database> {
+export function pureConnect (instance: Trilogy): Pool<Database> {
   return createPool({
     create () {
       return readDatabase(instance)
     },
 
-    async destroy (client: SqlJs.Database): Promise<void> {
+    async destroy (client: Database): Promise<void> {
       client.close()
     }
   }, { min: 1, max: 1 })
